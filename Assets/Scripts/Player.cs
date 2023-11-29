cc4cb9552638b0caea7d5a77bf3963a582324ffb
Psychiasis <jbage21@student.aau.dk>
2023-11-29 14:22:02 +0100

    More Attack
    Added attack box on left click and a dash for space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,8 +12,15 @@ public class Movement : MonoBehaviour
     public float speedLimit = 0.5f;
     public float moveSpeed;
 
-    Vector2 mousePos;
-    Vector2 mouseDis;
+    Vector2 mousePosition;
+    Vector2 moveDirection;
+
+    [SerializeField] float dashSpeed = 20f;
+    [SerializeField] float dashDuration = .1f;
+    [SerializeField] float dashCooldown = 5f;
+
+    bool isDashing;
+    bool canDash;
 
     //Camera is referenced to track mouse position in void update
     public Camera cam;
@@ -21,19 +28,33 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        canDash = true;
     }
 
     
     void Update()
     {
+
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        moveDirection = new Vector2(horizontal, vertical).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetButtonDown("Dash") && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         if (horizontal !=0 && vertical !=0)
         {
             horizontal *= speedLimit;
@@ -42,8 +63,20 @@ public class Movement : MonoBehaviour
 
         rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
 
-        Vector2 lookDir = mousePos - rb.position;
+        Vector2 lookDir = mousePosition - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg -90f;
         rb.rotation = angle;
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(moveDirection.x * dashSpeed, moveDirection.y * dashSpeed);
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
